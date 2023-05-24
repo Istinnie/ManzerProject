f25e066fc5ba64aa47415f05b48f54ec2738af73
Istinnie <n.istinnie.velonkasina@gmail.com>
2023-05-24 14:15:56 +0400

    server + database connexion + api test + post datas OK

---
+++ server/models/repas.model.js
@@ -0,0 +1,19 @@
+module.exports = (mongoose) => {
+  var schema = mongoose.Schema(
+    {
+      nom: String,
+      ingredient: String,
+      image: String,
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Repas = mongoose.model("repas", schema);
+  return Repas;
+};
--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
-//  https://www.mongodb.com/languages/mern-stack-tutorial
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const db = require("./models");
 
-import express from "express";
-import cors from "cors";
-
-import dotenv from "dotenv/config";
-import records from "./routes/record.js";
-
-const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
 
-app.use(express.json());
+app.use(cors(corsOptions));
 
-app.use("/record", records);
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
 
-// start the Express server
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenue sur Manzer.re" });
+});
+
+// connexion à la base de données
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connecté à la base de données !");
+  })
+  .catch((err) => {
+    console.log("Impossible de se connecter à la base de données !", err);
+    process.exit();
+  });
+
+// route
+require("./routes/repas.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server is running on port ${PORT}.`);
 });
